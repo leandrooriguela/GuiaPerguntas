dc52442d6a2142471606f0505112a8fe993cd912
leandrooriguela <leandrooriguela@hotmail.com>
2020-06-02 21:30:17 -0300

    Inicial

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+//Estou dizendo para o Express usar o EJS como View engine
+app.set('view engine','ejs');
+
+app.get("/:nome/:lang",(req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do programador",
+        inscritos: 8000,
+        msg: exibirMsg
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("App rodando!");
+})
